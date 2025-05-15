44e46b5d9b6f76b79e6fc08db30d3449d3456a08
MoglaVOS <yurinandrew.29@gmail.com>
2025-05-15 23:29:29 +0300

    Foodgram: Foodgram: update docker-compose, update main.yml and settings.py, rename .env.example 1.9

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -68,9 +68,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "django"),
-        "USER": os.getenv("POSTGRES_USER", "django"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
-        "HOST": os.getenv("DB_HOST", ""),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
