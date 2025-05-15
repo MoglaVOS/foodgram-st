2f5eb9a6224ae7de04abedf74c670bd7bd36cf0c
MoglaVOS <yurinandrew.29@gmail.com>
2025-05-16 00:04:44 +0300

    Foodgram: Foodgram: update docker-compose.yml and settings.py 1.12

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -69,8 +69,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "django"),
         "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
